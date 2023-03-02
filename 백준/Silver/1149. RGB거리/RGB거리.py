5399f9d1d67e1704f38589fc88e4bf527775ac13
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-03-02 13:44:55 +0900

    [Silver I] Title: RGB거리, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,8 @@
+n = int(input())
+data = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, len(data)):
+    data[i][0] = min(data[i-1][1], data[i-1][2]) + data[i][0]
+    data[i][1] = min(data[i-1][0], data[i-1][2]) + data[i][1]
+    data[i][2] = min(data[i-1][0], data[i-1][1]) + data[i][2]
+print(min(data[n-1][0], data[n-1][1], data[n-1][2]))
