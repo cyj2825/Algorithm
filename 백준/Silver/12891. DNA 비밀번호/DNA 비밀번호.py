3a79d35e0800b449f63c05f7cc2645d2c34f6d0a
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-03-25 17:32:18 +0900

    [Silver II] Title: DNA 비밀번호, Time: 456 ms, Memory: 52048 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -1,23 +1,24 @@
+from collections import deque
+
 S, P = map(int, input().split())
 DNA = list(input())
-check = list(map(int, input().split()))
-dic = {'A': check[0], 'C': check[1], 'G': check[2], 'T': check[3]}
-base = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+A, C, G, T = map(int, input().split())
+dic = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+left, right = 0, P-1
+arr = deque(DNA[left:right])
+for i in arr:      # 마지막 수는 제외하고 구성 => right+1로 하게 되면 마지막에 범위를 넘어가는 오류 발생하므로
+    dic[i] += 1
 count = 0
 
-for i in range(S-P+1):
-    state = True
-    if i == 0:
-        for j in range(P):
-            base[DNA[j]] += 1
-    else:
-        base[DNA[i+P-1]] += 1
-        base[DNA[i-1]] -= 1
-
-    for i in ('A', 'C', 'G', 'T'):
-        if base[i] < dic[i]:
-            state = False
-            break
-    if state:
+while right < S:
+    # 마지막 오른쪽 수 추가 => 구간 완성
+    dic[DNA[right]] += 1
+    
+    # 주어진 개수 이상인지 확인 
+    if dic['A'] >= A and dic['C'] >= C and dic['G'] >= G and dic['T'] >= T:
         count += 1
+    # 구간 한 칸 이동
+    dic[DNA[left]] -= 1    # 첫번째 수 제거
+    left += 1
+    right += 1
 print(count)
