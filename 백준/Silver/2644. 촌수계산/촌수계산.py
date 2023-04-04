ca8c73e71431544ba891dbacaf1e071b2c0cd8ad
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-04-04 20:59:29 +0900

    [Silver II] Title: 촌수계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,32 +1,23 @@
-# 너비 우선 탐색 사용
 import sys
-from collections import deque
+sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
 n = int(input())
-# A, B는 촌수를 계산해야 하는 서로 다른 사람의 번호
-A, B = map(int, input().split())
+A = [[] for _ in range(n+1)]
+a, b = map(int, input().split())
 m = int(input())
-# 그래프 데이터 저장 인접 리스트 초기화
-graph = [[] for _ in range(n+1)]
-# 촌수를 의미하는 탐색 depth
-visited = [0] * (n+1)
 
+def DFS(v):
+    for i in A[v]:
+        if check[i] == 0:
+            check[i] = check[v]+1
+            DFS(i)
+            
 for _ in range(m):
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
-
-def BFS(a, b):
-    myQueue = deque([a])
-    while myQueue:      # myQueue 값이 빌 때까지 while문 반복
-        node = myQueue.popleft()   # deque의 가장 왼쪽에 있는 원소를 deque에서 제거하고, 그 값을 리턴
-        if node == b:
-            return visited[b]
-        for i in graph[node]:
-            if visited[i] == 0:
-                myQueue.append(i)
-                visited[i] = visited[node] + 1
-    return -1
-                
-print(BFS(A, B))
+    s, e = map(int, input().split())
+    A[s].append(e)
+    A[e].append(s)
+    
+check = [0]*(n+1)
+DFS(a)
+print(check[b] if check[b] > 0 else -1)
