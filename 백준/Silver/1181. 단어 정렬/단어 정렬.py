f9dc8173a3c4996fb15a720c2f406da2f7dd73b9
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-04-12 21:47:59 +0900

    [Silver V] Title: 단어 정렬, Time: 3732 ms, Memory: 38616 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -8,12 +8,14 @@ q = []
 a = []
 for i in range(n):
     s = input()
+    # 입력 받은 s 값이 a 리스트에 존재하는지 확인 -> 존재할 경우 push하지 않는다
     if s in a:
         continue
     else:
         a.append(s)
         heapq.heappush(q, (len(s) - 1, s[:-1]))
 
+# 입력 받은 s 값 중 중복값이 존재하여 q의 값이 n이 아닐 수 있으므로 0~len(q)-1만큼 반복
 for i in range(len(q)):
     a, answer = heapq.heappop(q)
     print(answer)
