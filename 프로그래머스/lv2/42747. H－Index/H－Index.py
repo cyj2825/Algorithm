7ee4dd7e2409279261a785b85720bf9ddcf48b3b
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-04-14 19:43:26 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -4,6 +4,7 @@ def solution(citations):
     l = len(citations)
     
     for i in range(l):
+        # citations에서 존재하는 값만 h가 될 수 있는 것이 아님
         if citations[i] >= l-i:
             return l-i
     return 0
