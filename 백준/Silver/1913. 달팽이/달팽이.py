4300b9e8a5ece2c4809677d783edb23b400e8e91
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-04-20 14:26:55 +0900

    [Silver III] Title: 달팽이, Time: 1100 ms, Memory: 70036 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,73 +1,46 @@
+# 다시보기
+# 좌표 개념으로 생각하여 아래, 오른쪽, 위, 왼쪽을 순서대로 탐색 => 조건에 맞지 않으면 오른쪽으로 넘어가고 또 조건에 맞지 않으면 위쪽으로 넘어가고..
 import sys
 input = sys.stdin.readline
 n = int(input())
-data = [[0]*n for _ in range(n)]
-data[n//2][n//2] = 1
-num = 2
-x = n // 2
-y = n // 2
-# 좌표를 출력할 숫자 받아오기
-q = int(input())
-a = 0
-b = 0
-# 상, 하, 좌, 우로 이동
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+m = int(input())
+# 좌표값 채우기
+data = [[0] * n for _ in range(n)]
 
-while x != 0 or y != 0:
-    # 상, 하, 좌, 우로 값이 존재하는지 확인
-    pos = [0, 0, 0, 0]
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n:
-            if i == 0 and data[nx][ny] != 0:
-                pos[0] = 1
-            elif i == 1 and data[nx][ny] != 0:
-                pos[1] = 1
-            elif i == 2 and data[nx][ny] != 0:
-                pos[2] = 1
-            elif i == 3 and data[nx][ny] != 0:
-                pos[3] = 1
-        else:
-            continue
+# 아래, 오른, 위, 왼 순서로 이동
+# 달팽이 좌표값이 채워지는 규칙 : 특정값이 조건에 맞으면 아래, 조건에 맞지 않으면 오른쪽으로, 그 조건도 맞지 않으면 위쪽으로...
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+# (0, 0)에 넣을 값, 즉 n**2값(n**2부터 1까지 좌표를 채워가는 것이므로)
+num = n ** 2
+i = 0
+x, y = 0, 0
 
-    if sum(pos) == 0:
-        x -= 1
-        data[x][y] = num
-    elif sum(pos) == 1:
-        if pos[0] == 1:
-            y -= 1
-            data[x][y] = num
-        elif pos[1] == 1:
-            y += 1
-            data[x][y] = num
-        elif pos[2] == 1:
-            x += 1
-            data[x][y] = num
-        elif pos[3] == 1:
-            x -= 1
-            data[x][y] = num
-    elif sum(pos) == 2:
-        if pos[0] == 1 and pos[2] == 1:
-            x += 1
-            data[x][y] = num
-        elif pos[0] == 1 and pos[3] == 1:
-            y -= 1
-            data[x][y] = num
-        elif pos[1] == 1 and pos[2] == 1:
-            y += 1
-            data[x][y] = num
-        elif pos[1] == 1 and pos[3] == 1:
-            x -= 1
-            data[x][y] = num
-    num += 1
+while True:
+    data[x][y] = num
+    # 아래, 오른, 위, 왼 순서로 이동하기 위해
+    nx = x + dx[i]
+    ny = y + dy[i]
+
+    # nx, ny값이 0~n-1 값 사이를 벗어나거나 0이 아닌(이미 값이 들어가있는) 상태일 때는 아래, 오른, 위, 왼 순서 변경
+    if nx < 0 or ny < 0 or nx >= n or ny >= n or data[nx][ny] != 0:
+        i += 1
+        if i == 4:
+            i = 0
+        continue    # 밑에 코드들 수행하지 않고 while문 맨 앞으로 돌아감
+
+    x, y = nx, ny
+    # n**2부터 1까지 채우기 위해 값을 1씩 줄여가며 while문 반복
+    num -= 1
+    if num == 1:
+        break
+data[x][y] = 1
 
 for i in range(n):
     for j in range(n):
-        print(data[i][j], end = ' ')
-        if data[i][j] == q:
-            a = i+1
-            b = j+1
-    print()
+        # data 리스트를 돌면서 좌표값을 출력할 때 m 값을 갖고 있는 index값도 저장
+        if data[i][j] == m :
+            a, b = i+1, j+1
+    # data리스트 x값 한 줄씩 출력
+    print(*data[i])
 print(a, b)
