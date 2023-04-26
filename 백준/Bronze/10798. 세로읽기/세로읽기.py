85ee127a71c6bb3c8b6faf9f9e7abd37e3d07e2f
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-04-26 11:37:21 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,6 +1,6 @@
-words = [input() for i in range(5)]
+data = [input() for i in range(5)]
 
 for j in range(15):
     for i in range(5):
-        if j < len(words[i]):
-            print(words[i][j], end='')
+        if j < len(data[i]):
+            print(data[i][j], end='')
