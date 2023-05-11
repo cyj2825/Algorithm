b4611dd4c63da896915030c9ebdea7df524e97a7
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-05-11 15:39:53 +0900

    [Gold V] Title: 로봇 청소기, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,44 +1,43 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
-
-n,m = map(int,input().split())
-graph = []
+# 가로 길이 n, 세로 길이 m
+n, m = map(int, input().split())
+# 로봇 청소기의 위치 x, y와 바라보는 방향 pos
+x, y, pos = map(int, input().split())
+# 방문한 위치를 저장하기 위한 visited 리스트
 visited = [[0] * m for _ in range(n)]
-r,c,d = map(int,input().split())
+# 로봇 청소기 처음 위치 방문 처리
+visited[x][y] = 1
+# 각 장소의 상태값 받기
+data = [list(map(int, input().split())) for _ in range(n)]
+# 북, 동, 남, 서 방향으로 이동 => 반시계방향으로 90도 회전하기 때문
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+count = 1
 
-# d => 0,3,2,1 순서로 돌아야한다.
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-
-for _ in range(n):
-    graph.append(list(map(int,input().split())))
-
-# 처음 시작하는 곳 방문 처리
-visited[r][c] = 1
-cnt = 1
-
-while 1:
-    flag = 0
-    # 4방향 확인
+while True:
+    # 이동할 수 있는 방향이 존재하는지 확인
+    state = 0
+    # 0: 북, 1: 동, 2: 남, 3: 서
     for _ in range(4):
-        # 0,3,2,1 순서 만들어주기위한 식
-        nx = r + dx[(d+3)%4]
-        ny = c + dy[(d+3)%4]
-        # 한번 돌았으면 그 방향으로 작업시작
-        d = (d+3)%4
-        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+        # 0, 3, 2, 1 순서를 만들어주기 위함 => pos가 0일 경우 3이 나와 서쪽으로 이동
+        nx = x + dx[(pos + 3) % 4]
+        ny = y + dy[(pos + 3) % 4]
+        pos = (pos + 3) % 4
+        if 0 <= nx < n and 0 <= ny < m and data[nx][ny] == 0:
             if visited[nx][ny] == 0:
                 visited[nx][ny] = 1
-                cnt += 1
-                r = nx
-                c = ny
-                #청소 한 방향이라도 했으면 다음으로 넘어감
-                flag = 1
+                count += 1
+                state = 1         # 한 방향이라도 이동했으므로 이동할 수 있는 방향 존재하는 값 1로 변경
+                x, y = nx, ny
                 break
-    if flag == 0: # 4방향 모두 청소가 되어 있을 때,
-        if graph[r-dx[d]][c-dy[d]] == 1: #후진했는데 벽
-            print(cnt)
+
+    # 네 방향 모두 청소할 수 없을 때
+    if state == 0:
+        # 바라보는 방향을 유지한 채로 한 칸 후진했을 때 벽일 경우는 더이상 이동할 수 없으므로 count값 출력 후 작동 종료
+        if data[x - dx[pos]][y - dy[pos]] == 1:
+            print(count)
             break
+        # 바라보는 방향을 유지한 채로 한 칸 후진했을 때 벽이 아닐 경우
         else:
-            r,c = r-dx[d],c-dy[d]
+            x, y = x - dx[pos], y - dy[pos]
