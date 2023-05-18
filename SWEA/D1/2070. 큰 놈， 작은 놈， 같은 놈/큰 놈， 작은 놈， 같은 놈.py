fd18215923ddebdc99817a74f1889a4623cb6b8d
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-05-18 13:47:40 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 149 ms, Memory: 58,504 KB -BaekjoonHub

---
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.py
@@ -0,0 +1,11 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    a, b = map(int, input().split())
+    answer = ''
+    if a < b:
+        answer = '<'
+    elif a > b:
+        answer = '>'
+    else:
+        answer = '='
+    print("#{} {}".format(test_case, answer))
