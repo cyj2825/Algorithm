7552300cced3ce25586fc02def8b669f6b89b760
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-05-19 13:30:26 +0900

    [Silver II] Title: 부분수열의 합, Time: 264 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,16 +1,22 @@
-# 조합을 이용한 브루트포스 방식
-from itertools import combinations
+# 재귀함수를 이용한 백트래킹 방식
 import sys
 input = sys.stdin.readline
 n, s = map(int, input().split())
 data = list(map(int, input().split()))
 count = 0
-# combinations 함수를 사용하여 data 배열에서 구성할 수 있는 모든 조합을 구해서 해당 조합의 총합이 구하고자 하는 s값과 같은지 확인
-for i in range(1, n+1):
-    com = combinations(data, i)
 
-    for j in com:
-        if sum(j) == s:
-            count += 1
+def subset_sum(i, sub_sum):
+    global count
+    if i >= n:
+        return
+    sub_sum += data[i]
+    if sub_sum == s:
+        count += 1
+    
+    # 현재 data[i]를 선택한 경우
+    subset_sum(i+1, sub_sum)
+    # 현재 data[i]를 선택하지 않은 경우
+    subset_sum(i+1, sub_sum-data[i])
 
+subset_sum(0, 0)
 print(count)
