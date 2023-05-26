6d3c2147ce521f2a58be442674eb6ed3ca352fd8
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-05-26 12:46:09 +0900

    [unrated] Title: 광물 캐기, Time: 7.82 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.py
@@ -11,23 +11,21 @@ def DFS(idx, d, ir, s, minerals, f):
         # 현재까지 구한 값과 최솟값을 비교하여 갱신
         answer = min(answer, f)
         return
-    dp = 0
-    ip = 0
-    sp = 0
+    dfa, ifa, sfa = 0, 0, 0
     # 5개의 광물에 대해 더하기
     for i in range(idx, min(idx+5, len(minerals))):
-        dp += fatigue[0][dic[minerals[i]]]
-        ip += fatigue[1][dic[minerals[i]]]
-        sp += fatigue[2][dic[minerals[i]]]
+        dfa += fatigue[0][dic[minerals[i]]]
+        ifa += fatigue[1][dic[minerals[i]]]
+        sfa += fatigue[2][dic[minerals[i]]]
     # 다이아몬드를 캐는 경우
     if d:
-        DFS(idx+5, d-1, ir, s, minerals, f+dp)
+        DFS(idx+5, d-1, ir, s, minerals, f+dfa)
     # 철을 캐는 경우
     if ir:
-        DFS(idx+5, d, ir-1, s, minerals, f+ip)
+        DFS(idx+5, d, ir-1, s, minerals, f+ifa)
     # 돌을 캐는 경우
-    if sp:
-        DFS(idx+5, d, ir, s-1, minerals, f+sp)
+    if s:
+        DFS(idx+5, d, ir, s-1, minerals, f+sfa)
 
 def solution(picks, minerals):
     global answer
