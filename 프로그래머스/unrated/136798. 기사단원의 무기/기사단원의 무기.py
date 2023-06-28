2e6aaeca0b5adb7acfc904534c80c01cfa45212f
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-06-28 21:57:21 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.78 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,16 @@
+def solution(number, limit, power):
+    # answer = 0
+    result = []
+    # 각 기사단원의 정수 값에 대한 약수 개수 구하기
+    for i in range(1, number+1):
+        count = 0
+        for j in range(1, int(i**(1/2)) + 1):
+            if (i % j == 0):
+                count += 1
+                # j의 제곱값이 i가 아닐 경우 총 약수 2개를 더해준다
+                if ( (j**2) != i) : 
+                    count += 1
+        if count > limit:
+            count = power
+        result.append(count)
+    return sum(result)
