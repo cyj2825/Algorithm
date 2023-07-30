0c7da3578b51e73a6b5028c7c1f569f48b4479d0
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-07-30 18:17:30 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 212 ms, Memory: 14888 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		/*입력 방법 2
+		 * Scanner sc = new Scanner(System.in);
+		 * int n = sc.nextInt();
+		 */
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
