295bcc04818bc85808f9a7342c76c427789e0092
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2023-12-04 18:06:05 +0900

    [Bronze V] Title: 문자와 문자열, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String st = br.readLine();
+		int i = Integer.parseInt(br.readLine())-1;
+
+		System.out.println(st.charAt(i));
+		br.close();
+	}
+}
