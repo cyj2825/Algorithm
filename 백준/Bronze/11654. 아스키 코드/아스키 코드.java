e88868f21f83fc11dc386be1d3641718d652ce51
Yejin Cho <80473521+cyj2825@users.noreply.github.com>
2024-09-17 16:19:01 +0900

    [Bronze V] Title: 아스키 코드, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char s = br.readLine().charAt(0);
+		int x = (int)s;
+		System.out.println(x);
+	}
+}
